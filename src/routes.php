c171cc21060cd7a5dd40f41218eb72625f3d47b3
Liam Demafelix <hey@liam.ph>
2020-06-25 12:36:59 +0800

    Remove middleware from GET

--- src/routes.php
+++ src/routes.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middleware' => 'laravel-updater', 'prefix' => config('laravel-updater.route'), 'namespace' => 'Demafelix\LaravelUpdater\Controllers'], function () {
+Route::group(['prefix' => config('laravel-updater.route'), 'namespace' => 'Demafelix\LaravelUpdater\Controllers'], function () {
     Route::get('/', 'LaravelUpdaterController@index');
-    Route::post('/', 'LaravelUpdaterController@update');
+    Route::post('/', 'LaravelUpdaterController@update')->middleware('laravel-updater');
 });
